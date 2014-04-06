73f8995ef3a49b80be3d4ffe27ae236f125c8177
Gareth Rushgrove <gareth@morethanseven.net>
2014-04-06 19:08:19 +0100

    specify protocol for test

--- spec/webserver_spec.rb
+++ spec/webserver_spec.rb
@@ -3,6 +3,6 @@ require "httparty"
 describe "The web server" do
   it "should be listening on the open port" do
     port = ENV["PORT"] or 80
-    HTTParty.get("localhost:#{port}").code.should == 200
+    HTTParty.get("http://localhost:#{port}").code.should == 200
   end
 end
