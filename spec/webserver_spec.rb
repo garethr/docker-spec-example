9f1eb590aae42347080d4eb25de82fc3ed6c67ce
Gareth Rushgrove <gareth@morethanseven.net>
2014-04-06 19:30:46 +0100

    docker port may return more than I realised

--- spec/webserver_spec.rb
+++ spec/webserver_spec.rb
@@ -2,7 +2,7 @@ require "httparty"
 
 describe "The web server" do
   it "should be listening on the open port" do
-    port = ENV["PORT"] or 80
-    HTTParty.get("http://localhost:#{port}").code.should == 200
+    port = ENV["PORT"] or 'localhost:80'
+    HTTParty.get("http://#{port}").code.should == 200
   end
 end
